cec5b8281971b00fbba86c9425e80feb18d0a195
Cathy Pierce <zsrhpierce@gmail.com>
2023-06-27 15:34:32 -0600

    mod 2

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+const newHeader = document.createElement("h1");
+newHeader.setAttribute('id', 'victory');
+
+newHeader.textContent = "Cathy Pierce is the champion";
+document.body.append(newHeader);
